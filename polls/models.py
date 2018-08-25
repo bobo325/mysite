ea35987e6ac1d3a47a814fcca8807c173931cd0b
chenbo <1126531273@qq.com>
2018-08-25 11:32:06 +0800

    okok,给你个提交信息

--- polls/models.py
+++ polls/models.py
@@ -3,16 +3,20 @@ import datetime
 from django.db import models
 
 # Create your models here.
-from django.db.models import CharField, DateTimeField, ForeignKey, IntegerField
+from django.db.models import CharField, DateTimeField, ForeignKey, IntegerField, AutoField
 from django.utils import timezone
 
 
 class Question(models.Model):
+    id = AutoField(primary_key=True)  # 自增字段，指定主键
     question_text = CharField(max_length=255)
     pub_date = DateTimeField('date published')
     # 给模型增加 __str__() 方法是很重要的，
     # 这不仅仅能给你在命令行里使用带来方便，
     # Django 自动生成的 admin 里也使用这个方法来表示对象。
+
+    class Meta:
+        db_table = 'question'
 
     def __str__(self):
         return self.question_text
@@ -28,9 +32,13 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
+    id = AutoField(primary_key=True)   # 自增字段，指定主键
     question = ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = CharField(max_length=255)
     votes = IntegerField(default=0)
+
+    class Meta:
+        db_table = 'choice'
 
     def __str__(self):
         return self.choice_text
